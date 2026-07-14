7d826db0a05eecb1a35297fb4b54b8ccbd4d8982
Екатерина Дышкант <91475035+pai-puella@users.noreply.github.com>
2026-07-14 19:32:23 +0300

    Update products table schema with additional columns

--- sql/00_create_schema.sql
+++ sql/00_create_schema.sql
@@ -47,10 +47,13 @@ CREATE TABLE raw.orders (
 CREATE TABLE raw.products (
     product_id            TEXT PRIMARY KEY,
     product_category_name TEXT,
-    product_weight_g      INTEGER,
-    product_length_cm     INTEGER,
-    product_height_cm     INTEGER,
-    product_width_cm      INTEGER
+    product_name_length        INTEGER,
+    product_description_length INTEGER,
+    product_photos_qty         INTEGER,
+    product_weight_g           INTEGER,
+    product_length_cm          INTEGER,
+    product_height_cm          INTEGER,
+    product_width_cm           INTEGER
 );
 
 -- ------------------------------------------------------------
